dc842ebbbe7bb43bd7112535152746d269ae1325
unknown <jkoynok@jkoynok-PC.(none)>
2012-05-10 15:43:07 -0400

    added ko

---
+++ TechFest.Backbone/Controllers/KoController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TechFest.Backbone.Controllers
+{
+    public class KoController : Controller
+    {
+        public ActionResult Index()
+        {         
+            return View();
+        }
+
+        public ActionResult SingleBook()
+        {
+            var db = new TechFest.Backbone.Models.BooksEntities();
+            return View(db.Books.First());
+        }
+
+        public ActionResult BookCollection()
+        {
+            var db = new TechFest.Backbone.Models.BooksEntities();
+            return View(db.Books);
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
