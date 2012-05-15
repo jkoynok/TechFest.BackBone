84a661c07e8b7001367c0485d9febed993906b5e
unknown <jkoynok@jkoynok-PC.(none)>
2012-05-14 22:48:02 -0400

    added what not to do added non-observable field

---
+++ TechFest.Backbone/Scripts/bad-jquery.js
@@ -0,0 +1,26 @@
+﻿
+//spay-query
+
+$(document).ready(function () {
+
+    var Likes = 0;
+    var Title = 'Great Expectations';
+
+    $('#foo').click(function () {
+            //stuff
+            Likes = Likes+1;
+            UpdateLikes();
+    });    
+
+});
+
+function UpdateLikes() {
+    $('#foo').html(Title + '(' + Likes.toString() + ')');
+
+}
+
+//problems
+// - storing state in global scope -> no objects
+// - functions in global scope
+// - wiring up event handlers in document ready
+// - building up html strings
