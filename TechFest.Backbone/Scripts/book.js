f59bc5a28232f2d194bb1e73292a6063c35ceeb2
Jon Koynok <jkoynok@gmail.com>
2012-05-29 19:13:34 -0400

    changed single book

---
+++ TechFest.Backbone/Scripts/book.js
@@ -0,0 +1,7 @@
+﻿tf.RawBooks = [
+    { "id": 1, "Title": "RabbitMQ in Action", "PageCount": 200, "Likes": 0, "PubDate": "\/Date(1336104000000)\/" },
+    { "id": 2, "Title": "MacRuby in ActionLL", "PageCount": 272, "Likes": 0, "PubDate": "\/Date(1334721600000)\/"},
+    { "id": 3, "Title": "HTML5 for .NET Developers", "PageCount": 350, "Likes": 0, "PubDate": "\/Date(1294635600000)\/"},
+    { "id": 4, "Title": "Functional Programming in Scala", "PageCount": 325, "Likes": 0, "PubDate": "\/Date(1328072400000)\/"},
+    { "id": 5, "Title": "Node.js in Action", "PageCount": 125, "Likes": 0, "PubDate": "\/Date(1335844800000)\/" }
+    ]; 
